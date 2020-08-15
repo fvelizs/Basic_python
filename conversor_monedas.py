a619dfae2e12b57e3c960c6e70461e81ddfeb44b
Felipe Veliz <f.veliz.sanchez@gmail.com>
2020-08-15 17:09:25 -0400

    conversor v0.3 codigo modulado con funciones

--- conversor_monedas.py
+++ conversor_monedas.py
@@ -7,21 +7,24 @@ Bienvenido a su conversor favorito,
 
 por favor ingrese una opcion: """
 opc = int(input(menu))
+
+def conversor ():
+    pesos = int(input("ingrese cantidad de pesos que tiene: "))
+    resultado= round(pesos/valor_dolar,2)
+    print("usted tiene $"+ str(resultado)+" dolares")
+    
+
 if opc == 1:
-    pesos = int(input("ingrese cantidad de pesos que tiene: "))
     valor_dolar= 750
-    resultado= round(pesos/valor_dolar,2)
-    print("usted tiene $"+ str(resultado)+" dolares")
+    conversor()
+   
 elif opc == 2:
-    pesos = int(input("ingrese cantidad de pesos que tiene: "))
     valor_dolar= 3.5
-    resultado= round(pesos/valor_dolar,2)
-    print("usted tiene $"+ str(resultado)+" dolares")
+    conversor()
+
 elif opc == 3:
-    pesos = int(input("ingrese cantidad de pesos que tiene: "))
     valor_dolar= 25
-    resultado= round(pesos/valor_dolar,2)
-    print("usted tiene $"+ str(resultado)+" dolares")
+    conversor()  
 else:
     print(str(opc) + " no es una opcion valida, por favor ingrece una opcion valida")
 
