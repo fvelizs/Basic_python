1b2b728ba010575dbcd5db59e9f43a7ec1195036
Felipe Veliz <f.veliz.sanchez@gmail.com>
2020-08-15 18:03:18 -0400

    palindormo version del profe platzi

--- palindromo.py
+++ palindromo.py
@@ -1,7 +1,22 @@
-palabra = input("ingrese una palabra: ")
-palindromo = palabra [::-1]
-print(palindromo)
-if palabra==palindromo:
-    print("es un palindromo")
-else:
-    print("no es un palindromo")
+def palindromo(palabra):
+    palabra = palabra.replace(" ", "")
+    palabra = palabra.lower()
+    print (palabra)
+    inverso = palabra[::-1]
+    if inverso==palabra:
+        return True
+    else:
+        return False
+
+
+def run():
+    palabra = input("ingrese una palabra: ")
+    es_palindromo = palindromo(palabra)
+    if es_palindromo == True:
+        print("es palindromo")
+    else:
+        print("no es palindromo")
+  
+
+if __name__ == "__main__":
+    run()
