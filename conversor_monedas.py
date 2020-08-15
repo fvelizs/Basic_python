ab3000875b41c254d5ff9bdf918b7d7a981e7f71
Felipe Veliz <f.veliz.sanchez@gmail.com>
2020-08-15 16:31:46 -0400

    conversor de monedas con mas de un pais

--- conversor_monedas.py
+++ conversor_monedas.py
@@ -1,4 +1,27 @@
-pesos = int(input("ingrese cantidad depesos que tiene: "))
-valor_dolar= 750
-resultado= round(pesos/valor_dolar,2)
-print("usted tiene $"+ str(resultado)+" dolares")
+menu = """
+Bienvenido a su conversor favorito, 
+
+1- Pesos Chilenos a Dolares
+2- Soles peruanos a Dolares
+3- Pesos Mexicanos a Dolares
+
+por favor ingrese una opcion: """
+opc = int(input(menu))
+if opc == 1:
+    pesos = int(input("ingrese cantidad de pesos que tiene: "))
+    valor_dolar= 750
+    resultado= round(pesos/valor_dolar,2)
+    print("usted tiene $"+ str(resultado)+" dolares")
+elif opc == 2:
+    pesos = int(input("ingrese cantidad de pesos que tiene: "))
+    valor_dolar= 3.5
+    resultado= round(pesos/valor_dolar,2)
+    print("usted tiene $"+ str(resultado)+" dolares")
+elif opc == 3:
+    pesos = int(input("ingrese cantidad de pesos que tiene: "))
+    valor_dolar= 25
+    resultado= round(pesos/valor_dolar,2)
+    print("usted tiene $"+ str(resultado)+" dolares")
+else:
+    print(str(opc) + " no es una opcion valida, por favor ingrece una opcion valida")
+
