849b61a0aeb34e066a37f8003da809db2a6beef0
Felipe Veliz <f.veliz.sanchez@gmail.com>
2020-08-15 20:31:23 -0400

    ciclo for ejercicio tablas de multiplicar con while comentado

--- bucles.py
+++ bucles.py
@@ -1,13 +1,24 @@
+def tabla (menu):
+    contador=1
+    for i in range(10):
+        resultado= contador*menu
+        print (str(menu)+"*"+str(contador)+"= "+str(resultado))
+        contador+=1
+
+
 def run():
-    LIMITE = 10000
-    contador=0
-    resultado=0
+    #ejemplo ciclo while comentado
+#     LIMITE = 10000
+#     contador=0
+#     resultado=0
 
-    while (resultado < LIMITE):
-        resultado=2**contador
-        print ("2 elevado a " + str(contador) + " es igual a: "+ str(resultado))   
-        contador = contador+1
+#     while (resultado < LIMITE):
+#         resultado=2**contador
+#         print ("2 elevado a " + str(contador) + " es igual a: "+ str(resultado))   
+#         contador = contador+1
 
+    menu= int(input("""Ingrese la la tabla que desea conocer: """))
+    tabla(menu)
 
 if __name__ == '__main__':
     run()
